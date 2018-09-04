6f30aa0d046ad018e4e43350fd0f613fba94d4a5
mkumar73 <manishkr7003@gmail.com>
2018-09-04 23:20:09 +0200

    lstm model building

--- python/03.air_passenger_lstm.py
+++ python/03.air_passenger_lstm.py
@@ -19,6 +19,39 @@ dataframe = pd.read_csv('data/AirPassengers.csv', usecols=[1], header=0, engine=
 data = dataframe.values
 data = data.astype('float32')
 
-print(data[:5])
-plt.plot(data)
-plt.show()
+
+def scalar_transformation(data):
+    scalar = MinMaxScaler(feature_range=(0, 1))
+    transformed_data = scalar.fit_transform(data)
+    return transformed_data
+
+
+def train_test_split(data, fraction=0.7):
+    training = int(len(data)*fraction)
+    train = data[0:training, :]
+    test = data[training:, :]
+    return train, test
+
+
+def prepare_data(data, time_step=1):
+    dataX = []
+    dataY = []
+    for i in range(len(data)-time_step-1):
+        temp = data[i, :]
+        dataX.append(temp)
+        dataY.append(data[i+time_step, :])
+    return np.array(dataX), np.array(dataY)
+
+
+#
+# data = scalar_transformation(data)
+# print(data[:5])
+#
+# train, test = train_test_split(data, fraction=0.7)
+# print(len(train))
+# print(len(test))
+#
+# X, y = prepare_data(train, time_step=1)
+# print(len(X))
+# print(len(y))
+
