609cb2b66e080f6fdf73ec23d7167bced5daa3e8
mkumar73 <manishkr7003@gmail.com>
2018-09-07 00:08:03 +0200

    logging the various steps

--- python/03.air_passenger_window_lstm.py
+++ python/03.air_passenger_window_lstm.py
@@ -38,17 +38,21 @@ def prepare_data(data, look_back=3):
 df = pd.read_csv('data/AirPassengers.csv', usecols=[1], header=0, engine='python')
 data = df.values
 data = data.astype('float32')
+logger.info('Data loaded..!!')
 # print(data[:5])
 
 
 # split the data into train and test samples
 train, test = train_test_split(data, fraction=0.7)
+logger.info('Train test split done')
+
 # print(len(train))
 # print(len(test))
 
 # prepare the uni-variate data in the form of X, y
 trainX, trainY = prepare_data(train, look_back=3)
 testX, testY = prepare_data(test, look_back=3)
+logger.info('Data prepared as per window sequence format..')
 
 # print(trainX.shape)
 # print(trainY.shape)
@@ -65,6 +69,8 @@ testX = scalar_fitX.transform(testX)
 scalar_fitY = scalar.fit(trainY)
 trainY = scalar_fitY.transform(trainY)
 testY = scalar_fitY.transform(testY)
+logger.info('Scale transformation completed..')
+
 
 # create LSTM model
 time_step = 3
@@ -72,23 +78,33 @@ time_step = 3
 # reshape the input in the form of [samples, time step, features]
 trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+logger.info('Data reshaped for input to LSTM..')
 
+
+logger.info('Keras model building started..')
 model = Sequential()
 model.add(LSTM(5, activation='tanh', input_shape=(1, time_step)))
 model.add(Dense(1))
 model.summary()
 
 # training and model fitting
+logger.info('LSTM model training started..')
+
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(trainX, trainY, batch_size=1, epochs=100, verbose=2)
+
+logger.info('LSTM model training completed..')
 
 # prediction
 train_pred = model.predict(trainX)
 test_pred = model.predict(testX)
-print(train_pred.shape)
-print(train_pred[:5])
+logger.info('LSTM prediction completed..')
+
+# print(train_pred.shape)
+# print(train_pred[:5])
 
 # the results are in the form of scaled value, so inverse the transformation
+logger.info('Inverse transformation of prediction result..')
 train_pred_inverse = scalar_fitY.inverse_transform(train_pred)
 test_pred_inverse = scalar_fitY.inverse_transform(test_pred)
 trainY_inverse = scalar_fitY.inverse_transform(trainY)
@@ -100,29 +116,32 @@ testY_inverse = scalar_fitY.inverse_transform(testY)
 # logging.info('Test data prediction: {}\n'.format(test_pred_inverse[:5]))
 
 
-# # RMSE calculation
-# train_rmse = np.sqrt(mean_squared_error(trainY_inverse, train_pred_inverse))
-# test_rmse = np.sqrt(mean_squared_error(testY_inverse, test_pred_inverse))
+# RMSE calculation
+train_rmse = np.sqrt(mean_squared_error(trainY_inverse, train_pred_inverse))
+test_rmse = np.sqrt(mean_squared_error(testY_inverse, test_pred_inverse))
+
+logger.info('Training RMSE: {}'.format(train_rmse))
+logger.info('Test RMSE: {}'.format(test_rmse))
+
+# plotting the results and comparision
+# shift train predictions for plotting
+logger.info('Plotting the result for comparision..')
+
+train_plot = np.empty_like(data)
+train_plot[:, :] = np.nan
+train_plot[time_step:len(trainY_inverse)+time_step, :] = trainY_inverse
+
+# shift test predictions for plotting
+test_plot = np.empty_like(data)
+test_plot[:, :] = np.nan
+test_plot[len(trainY_inverse)+(time_step*2)+1:len(data)-1, :] = testY_inverse
 #
-# logger.info('Training RMSE: {}'.format(train_rmse))
-# logger.info('Test RMSE: {}'.format(test_rmse))
-# #
-# #
-# # plotting the results and comparision
-# # shift train predictions for plotting
-# train_plot = np.empty_like(data)
-# train_plot[:, :] = np.nan
-# train_plot[time_step:len(trainY_inverse)+time_step, :] = trainY_inverse
-#
-# # shift test predictions for plotting
-# test_plot = np.empty_like(data)
-# test_plot[:, :] = np.nan
-# test_plot[len(trainY_inverse)+(time_step*2)+1:len(data)-1, :] = testY_inverse
-# #
-# # plot baseline and predictions
-# plt.plot(data, 'r', label='data')
-# plt.plot(train_plot, 'g--', label='training')
-# plt.plot(test_plot, 'b:', label='test')
-# plt.legend(loc=0)
-# plt.savefig('plots/ap_lstm_window_result.jpg')
-# plt.show()
+# plot baseline and predictions
+plt.plot(data, 'r', label='data')
+plt.plot(train_plot, 'g--', label='training')
+plt.plot(test_plot, 'b:', label='test')
+plt.legend(loc=0)
+plt.savefig('plots/ap_lstm_window_result.jpg')
+plt.show()
+
+logger.info('Process completed..')
