e7cfb27ec29be4f03b1317f1ad7da454438c610e
mkumar73 <manishkr7003@gmail.com>
2018-09-09 14:35:01 +0200

    plt.style changed

--- python/random_walk.py
+++ python/random_walk.py
@@ -18,25 +18,26 @@ def tsplot(data, lags, show=True):
     if not isinstance(data, pd.Series):
         data = pd.Series(data)
 
-    layout = (3, 2)
+    with plt.style.context('bmh'):
+        layout = (3, 2)
 
-    ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
-    acf_ax = plt.subplot2grid(layout, (1, 0))
-    pacf_ax = plt.subplot2grid(layout, (1, 1))
-    qq_ax = plt.subplot2grid(layout, (2, 0))
-    prob_ax = plt.subplot2grid(layout, (2, 1))
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        qq_ax = plt.subplot2grid(layout, (2, 0))
+        prob_ax = plt.subplot2grid(layout, (2, 1))
 
-    data.plot(ax=ts_ax)
-    ts_ax.set_title('Line plot for the data')
-    smt.graphics.plot_acf(data, ax=acf_ax, lags=lags)
-    smt.graphics.plot_pacf(data, ax=pacf_ax, lags=lags)
-    sm.qqplot(data, line='s', ax=qq_ax)
-    qq_ax.set_title('QQ Plot')
-    scs.probplot(data, sparams=(data.mean(), data.std()), plot=prob_ax)
+        data.plot(ax=ts_ax)
+        ts_ax.set_title('Line plot for the data')
+        smt.graphics.plot_acf(data, ax=acf_ax, lags=lags)
+        smt.graphics.plot_pacf(data, ax=pacf_ax, lags=lags)
+        sm.qqplot(data, line='s', ax=qq_ax)
+        qq_ax.set_title('QQ Plot')
+        scs.probplot(data, sparams=(data.mean(), data.std()), plot=prob_ax)
 
-    plt.tight_layout()
-    if show:
-        plt.show()
+        plt.tight_layout()
+        if show:
+            plt.show()
 
     return
 
