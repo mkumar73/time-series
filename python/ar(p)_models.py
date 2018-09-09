1d31185c05a872362e4a22e0bcbcf418bec3ef56
mkumar73 <manishkr7003@gmail.com>
2018-09-09 17:48:03 +0200

    ar1 model

--- python/ar(p)_models.py
+++ python/ar(p)_models.py
@@ -10,4 +10,20 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import scipy.stats as scs
 
+# my utils library
+from ts_utils import tsplot
 
+np.random.seed(1)
+n_samples = 1000
+data = noise = np.random.normal(size=1000)
+
+# ar(1) model : x[t] = alpha * x[t-1] + noise
+
+# case : 1, alpha = 0.5
+alpha = 0.7
+data_1 = np.zeros(len(data))
+for t in range(len(data)):
+    data[t] = alpha * data[t-1] + noise[t]
+
+
+tsplot(data, lags=20, show=True)
